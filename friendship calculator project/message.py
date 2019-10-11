5648635b5d65898e0577a76ec515a0a092ed8124
Abdirashid.Abdi <abdirashidmahad52@gmail.com>
2019-10-11 09:26:26 -0400

    fucntion choice created

---
+++ friendship calculator project/message.py
@@ -0,0 +1,12 @@
+
+
+def choice_message():
+    messages = {
+        "first_ms": "It looks like they will no relation python",
+        "second_ms": "",
+        "third_ms": " z"
+    }
+    print(messages.get("third_ms"))
+
+
+choice_message()
